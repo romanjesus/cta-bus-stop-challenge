91e714b7239c3625421a6ceda45299712fa3f07a
Jesus Roman <jesusroman2013@gmail.com>
2016-11-13 11:55:14 -0600

    Add activerecord associations

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,2 +1,4 @@
 class Route < ActiveRecord::Base
+  has_many :routemapper
+  has_many :stops, { through: :routemapper, source: :stop}
 end
--- app/models/routemapper.rb
+++ app/models/routemapper.rb
@@ -1,2 +1,4 @@
 class Routemapper < ActiveRecord::Base
+  belongs_to :route
+  belongs_to :stop
 end
