b3bcb3dd750d76ea903ca91a03f1524e46156c77
Jesus Roman <jesusroman2013@gmail.com>
2016-11-13 10:15:20 -0600

    Migration file for bus stop bus routes join table

--- db/migrate/20161113160646_create_routemappers.rb
+++ db/migrate/20161113160646_create_routemappers.rb
@@ -2,6 +2,9 @@ class CreateRoutemappers < ActiveRecord::Migration
   def change
     create_table :routemappers do |t|
 
+      t.integer :stop_id
+      t.integer :route_id 
+
       t.timestamps null: false
     end
   end
