f4cddb1b79a835b36ffbdad875cf94496797226e
Jesus Roman <jesusroman2013@gmail.com>
2016-11-13 10:14:26 -0600

    Migration for route table

--- db/migrate/20161113160205_create_routes.rb
+++ db/migrate/20161113160205_create_routes.rb
@@ -2,6 +2,9 @@ class CreateRoutes < ActiveRecord::Migration
   def change
     create_table :routes do |t|
 
+      t.string :route_number
+      t.string :name
+
       t.timestamps null: false
     end
   end
