ee1844f1b2ef683b3beb74c736dd087b4958b7a5
Jesus Roman <jesusroman2013@gmail.com>
2016-11-14 12:40:05 -0600

    Add top ten buttons, routes, and pages along with functioning dropdown of routes

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -1,16 +1,8 @@
 class StopsController < ApplicationController
 
   def index
-    @stops_by_boardings = Stop.top_ten_by_boardings
-
-    @stops_by_alightings = Stop.top_ten_by_alightings
-
     @longest_route = Routemapper.longest_route
-
     @stop_w_most_routes = Routemapper.stop_w_most_routes
-
-    @stats = Stop.stop_stats(14011)
-
     @routes = Route.all
   end
 
@@ -18,6 +10,19 @@ class StopsController < ApplicationController
     @stop = Stop.find(params[:id])
     @stats = Stop.stop_stats(params[:id])
     render 'show'
+  end
+
+
+  def top_ten_by_boardings
+    @stops_by_boardings = Stop.top_ten_by_boardings
+
+    render 'top_ten_by_boardings'
+  end
+
+  def top_ten_by_alightings
+    @stops_by_alightings = Stop.top_ten_by_alightings
+
+    render 'top_ten_by_alightings'
   end
 
 end
