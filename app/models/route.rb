326f25134bafe078a1f515d1394a66f8d93a17e3
Jesus Roman <jesusroman2013@gmail.com>
2016-11-13 19:47:09 -0600

    Add methods for longest route and stop with most routes, room for improvement in terms of scalability

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,4 +1,9 @@
 class Route < ActiveRecord::Base
   has_many :routemapper
   has_many :stops, { through: :routemapper, source: :stop }
+
+  def num_of_stops
+    self.stops.count
+  end
+
 end
--- app/models/stop.rb
+++ app/models/stop.rb
@@ -1,4 +1,20 @@
 class Stop < ActiveRecord::Base
   has_many :routemapper
   has_many :routes, { through: :routemapper, source: :route }
+
+  def num_of_routes
+    self.routes.count
+  end
+
+  def clean_name
+    "#{self.on_street} & #{self.cross_street}"
+  end
+
+  def self.top_ten_by_boardings
+    self.order("boardings DESC").limit(10)
+  end
+
+  def self.top_ten_by_alightings
+    self.order("alightings DESC").limit(10)
+  end
 end
